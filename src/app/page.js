350321a92782f53964108cb7bbd61f56cef0e836
emrhncbn <emrhncbn@gmail.com>
2024-04-05 16:21:50 +0300

    Airbnb

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 "use client"
 
 import Airbnb from "@/components/Airbnb"
+import Data from "@/components/Data"
 
 
 const Home = () => {
   return (
     <div>
       <Airbnb />
+      <Data />
     </div>
   )
 }
